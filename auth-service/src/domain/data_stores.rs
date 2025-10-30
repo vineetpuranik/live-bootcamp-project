f87e70aed619f120a7d0c0922eac0eb7b4f87899
Vineet Puranik <vnt.manu@gmail.com>
2025-10-29 21:36:45 -0700

    Implement 2FA code store trait

--- auth-service/src/domain/data_stores.rs
+++ auth-service/src/domain/data_stores.rs
@@ -1,3 +1,8 @@
+use std::result;
+
+use rand::Rng;
+use uuid::Uuid;
+
 use super::{Email, Password, User};
 
 #[async_trait::async_trait]
@@ -25,4 +30,86 @@ pub enum UserStoreError {
 #[derive(Debug, PartialEq)]
 pub enum BannedTokenStoreError {
     UnexpectedError,
+}
+
+// This trait represents the interface all concrete 2FA code stores should represent
+#[async_trait::async_trait]
+pub trait TwoFACodeStore {
+    async fn add_code(
+        &mut self,
+        email: Email,
+        login_attempt_id: LoginAttemptId,
+        code: TwoFACode,
+    ) -> Result<(), TwoFACodeStoreError>;
+
+    async fn remove_code(&mut self, email: &Email) -> Result<(), TwoFACodeStoreError>;
+
+    async fn get_code(
+        &self,
+        email: &Email,
+    ) -> Result<(LoginAttemptId, TwoFACode), TwoFACodeStoreError>;
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TwoFACodeStoreError {
+    LoginAttemptIdNotFound,
+    UnexpectedError,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LoginAttemptId(String);
+
+impl LoginAttemptId {
+    pub fn parse(id: String) -> Result<Self, String> {
+        // Use the parse_str function from the uuid crate to ensure id is a valid UUID
+        match Uuid::parse_str(&id) {
+            Ok(_) => Ok(Self(id)),
+            Err(_) => Err("Invalid UUID".into()),
+        }
+    }
+}
+impl Default for LoginAttemptId {
+    fn default() -> Self {
+        // Use the `uuid` crate to generate a random version 4 UUID
+        Self(Uuid::new_v4().to_string())
+    }
+}
+
+// Rust does not know how to turn LoginAttemptId into a &str
+// that is why we implement the AsRef<str> trait
+// “This type can give you a &str view of itself.”
+// “Whenever someone needs a &str, they can call as_ref() on this type to get it.”
+impl AsRef<str> for LoginAttemptId {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct TwoFACode(String);
+
+impl TwoFACode {
+    pub fn parse(code: String) -> Result<Self, String> {
+        // Ensure `code` is a valid 6-digit code
+        if code.len() == 6 && code.chars().all(|c| c.is_ascii_digit()) {
+            Ok(Self(code))
+        } else {
+            Err("Not valid 6-digit code".into())
+        }
+    }
+}
+
+impl Default for TwoFACode {
+    fn default() -> Self {
+        // Use the `rand` crate to generate a random 2FAcode.
+        // The code should be 6 digits (ex: 834629)
+        let rand_6digits = rand::thread_rng().gen_range(100_000..=999_999);
+        Self(rand_6digits.to_string())
+    }
+}
+
+impl AsRef<str> for TwoFACode {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,5 +1,7 @@
 use crate::helpers::{get_random_email, TestApp};
-use auth_service::{utils::constants::JWT_COOKIE_NAME, ErrorResponse};
+use auth_service::{
+    routes::TwoFactorAuthResponse, utils::constants::JWT_COOKIE_NAME, ErrorResponse,
+};
 
 #[tokio::test]
 async fn should_return_422_if_malformed_credentials() {
@@ -176,4 +178,13 @@ async fn should_return_206_if_valid_credentials_and_2fa_enabled() {
 
     let response = app.post_login(&login_body).await;
     assert_eq!(response.status().as_u16(), 206);
+
+    assert_eq!(
+        response
+            .json::<TwoFactorAuthResponse>()
+            .await
+            .expect("Could not deserialize response body to TwoFactorAuthResponse")
+            .message,
+        "2FA required".to_owned()
+    );
 }
