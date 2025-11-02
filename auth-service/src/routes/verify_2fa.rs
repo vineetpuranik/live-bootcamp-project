7f9717e9563ddf4286214430b091d0dfa9007099
Vineet Puranik <vnt.manu@gmail.com>
2025-11-02 12:52:29 -0800

    Add Verify2FARequest struct for verify_2fa post method and 422 test case

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -1,5 +1,15 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
 
-pub async fn verify_2fa() -> impl IntoResponse {
+pub async fn verify_2fa(Json(request): Json<Verify2FARequest>) -> impl IntoResponse {
     StatusCode::OK.into_response()
+}
+
+#[derive(Deserialize)]
+pub struct Verify2FARequest {
+    pub email: String,
+    #[serde(rename = "loginAttemptId")]
+    pub login_attempt_id: String,
+    #[serde(rename = "2FACode")]
+    pub two_fa_code: String,
 }
