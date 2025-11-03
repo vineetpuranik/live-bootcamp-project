9acf5f221206ea3445e949dedd569fb0ce3d3865
Vineet Puranik <vnt.manu@gmail.com>
2025-11-02 22:54:27 -0800

    Add Postgres docker container _ sqlx rust crate

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,4 +1,6 @@
 use auth_service::services::{HashMapTwoFACodeStore, HashsetBannedTokenStore, MockEmailClient};
+use auth_service::utils::DATABASE_URL;
+use sqlx::PgPool;
 use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 use tokio::sync::RwLock;
@@ -6,9 +8,12 @@ use tokio::sync::RwLock;
 use auth_service::app_state::AppState;
 use auth_service::domain::UserStore;
 use auth_service::{services::HashMapUserStore, utils::constants::prod, Application};
+use auth_service::get_postgres_pool;
 
 #[tokio::main]
 async fn main() {
+    let pg_pool = configure_postgresql().await;
+
     let user_store: Box<dyn UserStore + Send + Sync> = Box::new(HashMapUserStore {
         users: HashMap::new(),
     });
@@ -40,4 +45,19 @@ async fn main() {
         .expect("Failed to build app");
 
     app.run().await.expect("Failed to run the app");
+}
+
+async fn configure_postgresql() -> PgPool {
+    // Create a new database connection pool
+    let pg_pool = get_postgres_pool(&DATABASE_URL)
+        .await
+        .expect("Failed to create Postgres connection pool!");
+
+    // Run database migrations against our test database! 
+    sqlx::migrate!()
+        .run(&pg_pool)
+        .await
+        .expect("Failed to run migrations");
+
+    pg_pool
 }
